03adc99f18ae3e24bfbdd3fbdae204d146e5ae11
jack-lundquist <31962755+jack-lundquist@users.noreply.github.com>
2018-02-20 23:18:42 -0500

    partial update

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,31 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-$.getJSON('data/dallas3.geojson', function(dallas) {
-  L.geoJSON(dallas, {
+L.geoJSON(dallas3, {
     style: {
       // dashArray: '3 10',
       color: 'black',
       fillOpacity: 0,
     }
   }).addTo(map);
+
+
+dallas3.forEach(function(dallas) {
+  var latLon = [dallas.lat, dallas.lon];
+
+  var color = '#FFF';
+
+  var options = {
+    radius: 6,
+    opacity: 1,
+    fillColor: color,
+    fillOpacity: 0.9,
+    color: '#FFF',
+    weight: 2,
+  };
+  var marker = L.circleMarker(latLon, options)
+      .bindPopup(dallas.violation, {offset: [0, -6]})
+      .addTo(map)
+  // add the marker to the markersArray
+  markersArray.push(marker);
 });
